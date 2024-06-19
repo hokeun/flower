e61c4766dee7553caab417e337c5df0671ccb315
Hokeun Kim <hokeunkim@berkeley.edu>
2024-06-18 19:18:52 -0700

    Add a new script for running 50 rounds

---
+++ examples/app-pytorch/run_experiments_50_rounds.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Make sure to run poetry shell first!
+
+date_time=$(date '+%Y_%m_%d_%H%M%S');
+
+HOKEUN_FLWR_NUM_ROUNDS=50 HOKEUN_FLWR_NOISE_ENABLED=0 HOKEUN_FLWR_GAUSS_NOISE_SIGMA=0.0 bash -c 'flower-simulation --server-app server:app --client-app client:app --num-supernodes 5' 2>&1 | tee "$date_time"_results_50_R_5_C_0_NOISE.txt
+
+HOKEUN_FLWR_NUM_ROUNDS=50 HOKEUN_FLWR_NOISE_ENABLED=1 HOKEUN_FLWR_GAUSS_NOISE_SIGMA=1.0 bash -c 'flower-simulation --server-app server:app --client-app client:app --num-supernodes 5' 2>&1 | tee "$date_time"_results_50_R_5_C_1.0_NOISE.txt
+
+HOKEUN_FLWR_NUM_ROUNDS=50 HOKEUN_FLWR_NOISE_ENABLED=1 HOKEUN_FLWR_GAUSS_NOISE_SIGMA=0.5 bash -c 'flower-simulation --server-app server:app --client-app client:app --num-supernodes 5' 2>&1 | tee "$date_time"_results_50_R_5_C_0.5_NOISE.txt
+
+HOKEUN_FLWR_NUM_ROUNDS=50 HOKEUN_FLWR_NOISE_ENABLED=1 HOKEUN_FLWR_GAUSS_NOISE_SIGMA=0.1 bash -c 'flower-simulation --server-app server:app --client-app client:app --num-supernodes 5' 2>&1 | tee "$date_time"_results_50_R_5_C_0.1_NOISE.txt
+
+HOKEUN_FLWR_NUM_ROUNDS=50 HOKEUN_FLWR_NOISE_ENABLED=1 HOKEUN_FLWR_GAUSS_NOISE_SIGMA=0.05 bash -c 'flower-simulation --server-app server:app --client-app client:app --num-supernodes 5' 2>&1 | tee "$date_time"_results_50_R_5_C_0.05_NOISE.txt
+
+HOKEUN_FLWR_NUM_ROUNDS=50 HOKEUN_FLWR_NOISE_ENABLED=1 HOKEUN_FLWR_GAUSS_NOISE_SIGMA=0.01 bash -c 'flower-simulation --server-app server:app --client-app client:app --num-supernodes 5' 2>&1 | tee "$date_time"_results_50_R_5_C_0.01_NOISE.txt
+
+
+
+
+HOKEUN_FLWR_NUM_ROUNDS=50 HOKEUN_FLWR_NOISE_ENABLED=0 HOKEUN_FLWR_GAUSS_NOISE_SIGMA=0.0 bash -c 'flower-simulation --server-app server:app --client-app client:app --num-supernodes 10' 2>&1 | tee "$date_time"_results_50_R_10_C_0_NOISE.txt
+
+HOKEUN_FLWR_NUM_ROUNDS=50 HOKEUN_FLWR_NOISE_ENABLED=1 HOKEUN_FLWR_GAUSS_NOISE_SIGMA=1.0 bash -c 'flower-simulation --server-app server:app --client-app client:app --num-supernodes 10' 2>&1 | tee "$date_time"_results_50_R_10_C_1.0_NOISE.txt
+
+HOKEUN_FLWR_NUM_ROUNDS=50 HOKEUN_FLWR_NOISE_ENABLED=1 HOKEUN_FLWR_GAUSS_NOISE_SIGMA=0.5 bash -c 'flower-simulation --server-app server:app --client-app client:app --num-supernodes 10' 2>&1 | tee "$date_time"_results_50_R_10_C_0.5_NOISE.txt
+
+HOKEUN_FLWR_NUM_ROUNDS=50 HOKEUN_FLWR_NOISE_ENABLED=1 HOKEUN_FLWR_GAUSS_NOISE_SIGMA=0.1 bash -c 'flower-simulation --server-app server:app --client-app client:app --num-supernodes 10' 2>&1 | tee "$date_time"_results_50_R_10_C_0.1_NOISE.txt
+
+HOKEUN_FLWR_NUM_ROUNDS=50 HOKEUN_FLWR_NOISE_ENABLED=1 HOKEUN_FLWR_GAUSS_NOISE_SIGMA=0.05 bash -c 'flower-simulation --server-app server:app --client-app client:app --num-supernodes 10' 2>&1 | tee "$date_time"_results_50_R_10_C_0.05_NOISE.txt
+
+HOKEUN_FLWR_NUM_ROUNDS=50 HOKEUN_FLWR_NOISE_ENABLED=1 HOKEUN_FLWR_GAUSS_NOISE_SIGMA=0.01 bash -c 'flower-simulation --server-app server:app --client-app client:app --num-supernodes 10' 2>&1 | tee "$date_time"_results_50_R_10_C_0.01_NOISE.txt
+
+
+
+
+HOKEUN_FLWR_NUM_ROUNDS=50 HOKEUN_FLWR_NOISE_ENABLED=0 HOKEUN_FLWR_GAUSS_NOISE_SIGMA=0.0 bash -c 'flower-simulation --server-app server:app --client-app client:app --num-supernodes 20' 2>&1 | tee "$date_time"_results_50_R_20_C_0_NOISE.txt
+
+HOKEUN_FLWR_NUM_ROUNDS=50 HOKEUN_FLWR_NOISE_ENABLED=1 HOKEUN_FLWR_GAUSS_NOISE_SIGMA=1.0 bash -c 'flower-simulation --server-app server:app --client-app client:app --num-supernodes 20' 2>&1 | tee "$date_time"_results_50_R_20_C_1.0_NOISE.txt
+
+HOKEUN_FLWR_NUM_ROUNDS=50 HOKEUN_FLWR_NOISE_ENABLED=1 HOKEUN_FLWR_GAUSS_NOISE_SIGMA=0.5 bash -c 'flower-simulation --server-app server:app --client-app client:app --num-supernodes 20' 2>&1 | tee "$date_time"_results_50_R_20_C_0.5_NOISE.txt
+
+HOKEUN_FLWR_NUM_ROUNDS=50 HOKEUN_FLWR_NOISE_ENABLED=1 HOKEUN_FLWR_GAUSS_NOISE_SIGMA=0.1 bash -c 'flower-simulation --server-app server:app --client-app client:app --num-supernodes 20' 2>&1 | tee "$date_time"_results_50_R_20_C_0.1_NOISE.txt
+
+HOKEUN_FLWR_NUM_ROUNDS=50 HOKEUN_FLWR_NOISE_ENABLED=1 HOKEUN_FLWR_GAUSS_NOISE_SIGMA=0.05 bash -c 'flower-simulation --server-app server:app --client-app client:app --num-supernodes 20' 2>&1 | tee "$date_time"_results_50_R_20_C_0.05_NOISE.txt
+
+HOKEUN_FLWR_NUM_ROUNDS=50 HOKEUN_FLWR_NOISE_ENABLED=1 HOKEUN_FLWR_GAUSS_NOISE_SIGMA=0.01 bash -c 'flower-simulation --server-app server:app --client-app client:app --num-supernodes 20' 2>&1 | tee "$date_time"_results_50_R_20_C_0.01_NOISE.txt
